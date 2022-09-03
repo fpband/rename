ff330dc5d8685143367e3ef93d46f643796c7d34
HYBRID <48980248+hybridvamp@users.noreply.github.com>
2022-09-04 00:29:27 +0400

    hybridation

--- bot/core/new/upload_document.py
+++ bot/core/new/upload_document.py
@@ -56,7 +56,7 @@ class UploadDocument:
 
         if not caption:
             caption = f"**File Name:** `{os.path.basename(document)}`" \
-                      "\n\n**@AH_RenameBot**"
+                      "\n\n**@HYBRID_Bots**"
         c_time = time.time()
         await self.send_document(
             chat_id=chat_id,
--- bot/plugins/ping.py
+++ bot/plugins/ping.py
@@ -13,7 +13,7 @@ async def ping_handler(c: Client, m: "types.Message"):
     await add_user_to_database(c, m)
     await c.send_flooded_message(
         chat_id=m.chat.id,
-        text="Hi, I am Rename Bot!\n\n"
+        text="Hi, I am Rename Bot!\n"
              "I can rename media without downloading it!\n"
              "Speed depends on your media DC.\n\n"
              "Just send me media and reply to it with /rename command.",
@@ -33,9 +33,9 @@ async def help_handler(c: Client, m: "types.Message"):
         chat_id=m.chat.id,
         text="I can rename media without downloading it!\n"
              "Speed depends on your media DC.\n\n"
-             "Just send me media and reply to it with /rename command.\n\n"
-             "To set custom thumbnail reply to any image with /set_thumbnail\n\n"
-             "To see custom thumbnail press /show_thumbnail",
+             "● Just send me media and reply to it with /rename command.\n"
+             "● To set custom thumbnail reply to any image with /set_thumbnail\n"
+             "● To see custom thumbnail press /show_thumbnail",
         reply_markup=types.InlineKeyboardMarkup([[
            types.InlineKeyboardButton("Show Settings",
                                       callback_data="showSettings")]])
